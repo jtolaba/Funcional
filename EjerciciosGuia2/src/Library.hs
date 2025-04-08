c00638e1f77ba369f5ec4ac92fca48276ca1a430
Jorge Ismael Tolaba <jtolaba@frba.utn.edu.ar>
2025-04-08 18:07:33 +0000

    Corrige la definición de esMultiploDe y agrega funciones para verificar años bisiestos.

--- EjerciciosGuia2/src/Library.hs
+++ EjerciciosGuia2/src/Library.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 module Library where
 import PdePreludat
 import Test.Hspec (xcontext)
@@ -25,8 +26,26 @@ esNumeroPositivo :: Number -> Bool
 esNumeroPositivo = (>0)
 
 --- Ejercicio 6 ---
-esMultiploDe :: Number -> Bool
-esMultiploDe = (0==). (`mod` 2)
+esMultiploDe :: Number -> Number -> Bool
+esMultiploDe dividendo = (==0).(dividendo `mod`)
 
 --- Ejercicio 7 ---
-esBisiesto = ()
+-- Un año es bisiesto si es divisible por 400 o es divisible por 4 pero no es divisible por 100
+esBisiesto :: Number -> Bool
+esBisiesto año = (||) (año`esMultiploDe` 400) ((&&) (año`esMultiploDe` 4) (not (año`esMultiploDe` 100)))
+
+esBisiesto2 :: Number -> Bool
+esBisiesto2 año =  (&&) ((`esMultiploDe`4) año) (not$(`esMultiploDe`100) año)|| (`esMultiploDe`400) año
+
+--- Ejercicio 8 ---
+
+inversaRaizCuadrada :: Number -> Number
+inversaRaizCuadrada = sqrt . inversa
+
+--- Ejercicio 9 ---
+
+incrementaMCuadradoN :: Number -> Number -> Number
+incrementaMCuadradoN m n = (+n).(^2)$m
+
+esResultadoPar :: Number -> Number -> Bool
+esResultadoPar n m= (==0).(`mod`2).(^m)$n
