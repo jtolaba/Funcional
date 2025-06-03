60bc93125db161d27c7a9f5af1e14aaff94795fa
Jorge Tolaba <jtolaba@frba.utn.edu.ar>
2025-06-03 12:48:50 -0300

    Parcial VonBoyage Resolucion

--- Parciales/BonVoyage/src/Library.hs
+++ Parciales/BonVoyage/src/Library.hs
@@ -1,11 +1,6 @@
 module Library where
 import PdePreludat
 
-
-
-doble :: Number -> Number
-doble numero = numero + numero
-
 data Reserva = Reserva {
   pasajeros :: [String],
   tramos :: [Tramo],
@@ -19,21 +14,26 @@ data Tramo = Tramo{
   duracion :: (Number,String)
 }deriving (Show)
 
---------------------------------------------------------------- Punto 2 ---------------------------------------------------------------
+---------------------------------------------------- Punto 2 ----------------------------------------------------
 reservaLarga = Reserva
   ["Tom Borenstyn","Frank Gorek"]  --- Pasajeros
-  [Tramo "Buenos Aires" "Sao Paulo" (6,"hs"), Tramo "Sao Paulo" "Londres" (10,"hs"),Tramo "Buenos Aires" "Chascomus" (25,"min"),Tramo "Buenos Aires" "Chascomus" (15,"min")] --- Tramos
+  [Tramo "Buenos Aires" "Sao Paulo" (6,"hs"), Tramo "Sao Paulo" "Londres" (10,"hs"),Tramo "Londres" "Paris" (25,"min"),Tramo "Paris" "Chascomus" (15,"min")] --- Tramos
   [lunchCompleto, menuEspecial "celiaco",otrosUtensillos ["almohada","auriculares"]] --- Agregados
   45000 --- Costo Base
-  
+
+reservaSinTramo = Reserva
+  ["Cesar Frere"] --- Pasajeros
+  [] --- Tramos
+  [lunchCompleto,menuEspecial "celiaco", polizon "Jorge Tolaba", equipajeExtra 10] --- Agregados
+  50000 --- Costo Base/home/jit/.local/bin/
 
 reservaCorta = Reserva
   ["Cesar Frere"] --- Pasajeros
-  [Tramo "Buenos Aires" "Chascomus" (15,"min")] --- Tramos
+  [Tramo "Buenos Aires" "Chascomus" (15,"min"),Tramo "Cordoba" "Saantiago" (12,"hs")] --- Tramos
   [lunchCompleto,menuEspecial "celiaco", polizon "Jorge Tolaba", equipajeExtra 10] --- Agregados
-  50000 --- Costo Base
+  50000 --- Costo Base/home/jit/.local/bin/
 
---------------------------------------------------------------- Punto 3 ---------------------------------------------------------------
+---------------------------------------------------- Punto 3 ----------------------------------------------------
 
 type Agregado = Reserva -> Reserva
 
@@ -43,17 +43,19 @@ modificarCostoBase modificador reserva = reserva { costoBase = (modificador . co
 lunchCompleto :: Agregado
 lunchCompleto = modificarCostoBase (*1.15)
 
-menuEspecial :: [a] -> Agregado
+menuEspecial :: String -> Agregado
 menuEspecial descripcion = modificarCostoBase (+ costoMenuEspecial)
   where
-    costoMenuEspecial = length descripcion * 10
+    costoMenuEspecial = length descripcion * multiplicadorDeCosto
+    multiplicadorDeCosto = 10  
 
 equipajeExtra :: Number -> Agregado
 equipajeExtra cantidad = modificarCostoBase (+ costoEquipajeExtra)
   where
-    costoEquipajeExtra = cantidad * 200
+    costoEquipajeExtra = cantidad * costoEquipaje
+    costoEquipaje = 200
 
-otrosUtensillos :: [a] -> Agregado
+otrosUtensillos :: [String] -> Agregado
 otrosUtensillos lista reserva = reserva { costoBase = costoBase reserva + costoUtensillos}
   where
     costoUtensillos = (length lista * costoBase reserva)/100
@@ -61,15 +63,20 @@ otrosUtensillos lista reserva = reserva { costoBase = costoBase reserva + costoU
 polizon :: String -> Agregado
 polizon pasajero reserva = reserva {pasajeros = pasajero:pasajeros reserva}
 
---------------------------------------------------------------- Punto 4 ---------------------------------------------------------------
+---------------------------------------------------- Punto 4 ----------------------------------------------------
 -- Implementar funciones que:
-
+-- Sume un monto al costo base de una reserva.
+aumentarCostoBase :: Number -> Reserva -> Reserva
+aumentarCostoBase monto = modificarCostoBase (+monto) 
 -- Sume un agregado a una reserva.
-
+sumarAgregado :: Agregado -> Reserva -> Reserva
+sumarAgregado agregado reserva = reserva {agregados = agregados reserva ++ [agregado]}
 -- Sume un tramo a una reserva.
 añadirTramo :: Tramo -> Reserva -> Reserva
-añadirTramo tramo reserva = reserva {tramos = tramos reserva ++ [tramo]}  
---------------------------------------------------------------- Punto 5 ---------------------------------------------------------------
+añadirTramo tramo reserva = reserva {tramos = tramos reserva ++ [tramo]}
+
+
+---------------------------------------------------- Punto 5 ----------------------------------------------------
 
 procesarAgregado :: [Agregado] -> Reserva -> Reserva
 procesarAgregado agregados reserva = foldl (\reservaActual agregado -> agregado reservaActual) reserva agregados
@@ -77,9 +84,11 @@ procesarAgregado agregados reserva = foldl (\reservaActual agregado -> agregado 
 precioFinal :: Reserva -> Reserva
 precioFinal reserva = procesarAgregado (agregados reserva) reserva
 
---------------------------------------------------------------- Punto 6 ---------------------------------------------------------------
+---------------------------------------------------- Punto 6 ----------------------------------------------------
 esUnaReservaLarga :: Reserva -> Bool
-esUnaReservaLarga = (>6) . tiempoTotalReserva
+esUnaReservaLarga = (> umbralDuracionCorta) . tiempoTotalReserva
+  where
+    umbralDuracionCorta = 15
 
 tiempoTotalReserva :: Reserva -> Number
 tiempoTotalReserva reserva= sum . map fst $ (map pasarDeMinutosAHoras . (\a -> map duracion a) . tramos) reserva
@@ -92,9 +101,24 @@ pasarDeMinutosAHoras (tiempo,tipo)
   | tipo == "min" = (tiempo / 60,"hs")
   | otherwise = (tiempo,tipo)
 
---------------------------------------------------------------- Punto 7 ---------------------------------------------------------------
+---------------------------------------------------- Punto 7 ----------------------------------------------------
 -- nuevaEscala :: Tramo -> Number -> Reserva
 nuevaEscala :: Tramo -> Number -> Reserva -> Reserva
-nuevaEscala tramo costo = modificarCostoBase (+costo) . añadirTramo tramo
+nuevaEscala tramo costo = aumentarCostoBase costo . añadirTramo tramo
 
---------------------------------------------------------------- Punto 8 ---------------------------------------------------------------
+---------------------------------------------------- Punto 8 ----------------------------------------------------
+
+reservaBienConstruida :: Reserva -> Bool
+reservaBienConstruida reserva = all tramoEnlazadoCorrectamente (zip (tramos reserva) ((tail.tramos) reserva)) && (not.null.tramos) reserva
+
+
+tramoEnlazadoCorrectamente :: (Tramo, Tramo) -> Bool
+tramoEnlazadoCorrectamente (tramoA, tramoB) = destino tramoA == origen tramoB
+
+---------------------------------------------------- Punto 9 ----------------------------------------------------
+
+-- a. Obtener el precio total de reservaLarga, agregándole un snack y una manta.
+-- (precioFinal.sumarAgregado (otrosUtensillos ["snack","manta"])) reservaLarga
+
+-- b. Agregarle a reservaLarga las escalas: Miami a Barcelona (11 hs., $15.000), y Barcelona a Roma (2 hs., $4.000), y saber si la reserva quedó bien construída.
+-- (reservaBienConstruida . nuevaEscala (Tramo "Barcelona" "Roma" (2,"hs")) 4000 . nuevaEscala (Tramo "Miami" "Barcelona" (11,"hs")) 15000) reservaLarga 
